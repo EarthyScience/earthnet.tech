b154d42acd13baa47f91304e4d3d0aced356327b
Lazaro Alonso <lazarus.alon@gmail.com>
2025-07-16 22:04:52 +0200

    support markdown tables syntax

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -14,6 +15,10 @@ console.log('Current NODE_ENV:', process.env.NODE_ENV);
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
 })
  
 // Merge MDX config with Next.js config
