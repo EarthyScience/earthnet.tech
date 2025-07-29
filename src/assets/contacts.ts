8c47c7f3752c4863b417f5515e5a94b08ad4f826
vitusbenson <vitus.benson@icloud.com>
2025-07-29 11:43:04 +0200

    adding imprint etc

--- src/assets/contacts.ts
+++ src/assets/contacts.ts
@@ -1,11 +1,11 @@
 import { AddressData, ContactData } from '@/components/FooterTypes';
 
 export const contactInfo = {
-  copyright: "EarthNet team",
+  copyright: "EarthNet Team",
   contact: {
     address: {
-      company: "Max-Planck Institute for Biogeochemistry",
-      street: "Hans-Knöll Str. 10",
+      company: "Max Planck Institute for Biogeochemistry",
+      street: "Hans-Knöll-Str. 10",
       zip: "07745",
       city: "Jena",
       state: "Thuringia",
@@ -15,7 +15,7 @@ export const contactInfo = {
     data: {
       contacts: [
         {
-          name: "team",
+          name: "",
           // position: "Researcher",
           email: "earthnet@bgc-jena.mpg.de"
         },
