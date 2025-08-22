65b0d7b7c1f681398e0e7db80467a36f6b841931
Lazaro Alonso <lazarus.alon@gmail.com>
2025-08-22 11:17:52 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export' as const,
-  basePath: process.env.NODE_ENV === 'production' ? '/earthnet.tech' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
   },
